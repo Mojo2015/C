7ecb36fd5a2558942c7f1305fa8d3ec84f4e2e01
Mojo2015 <lemongelloj@gmail.com>
2014-12-06 22:26:19 -0500

    Ubuntu C

---
+++ Harvard - CS50/Readme.txt
@@ -0,0 +1,2 @@
+Harvard Course - CS50 (Introduction to Computer Science)
+Homework assignments and self study projects.
---
+++ Harvard - CS50/forloop.c
@@ -0,0 +1,22 @@
+//While loop - Terminates at 100,000 (don't run this if 
+//your computer is slow)
+
+
+#include <stdio.h>
+
+int main()
+
+{
+	int i;
+	int counter = 0;
+	while (counter < 100000)
+	
+	{
+	
+		//printf("Oh Hai World\n");
+		printf("%d\n", counter);
+		counter++;
+		
+	}
+	
+}
